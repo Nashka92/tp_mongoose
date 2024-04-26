857c7add179cf963697259178da6b34acbcb2d25
Anna RAACH <annagraph92@gmail.com>
2024-04-26 12:01:19 +0200

    remove unless comment

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movieController");
 
-// Ici on définit les routes associées aux fonctions du bookController
 router.post("/add-movie", movieController.addMovie);
 router.get("/all-movie", movieController.findMovie);
 router.get("/movie-title/:title", movieController.findMovieByTitle);
