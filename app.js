8582c2293a4ab6c46f00b630d9ac05d22c4183a5
Anna RAACH <annagraph92@gmail.com>
2024-04-26 11:54:53 +0200

    tp(mongoose): install and add cors

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 const movieRoutes = require("./routes/movieRoutes")
+const cors = require("cors")
 
+app.use(cors)
 app.use(express.json());
 app.use(movieRoutes);
 
